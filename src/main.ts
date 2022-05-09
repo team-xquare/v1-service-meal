2c6e3426234e771937a94529fe1a0edc37cf15a3
정지우 <jiwoourty@gmail.com>
2022-05-09 20:12:26 +0900

    ⚡️ :: add cros option, validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
