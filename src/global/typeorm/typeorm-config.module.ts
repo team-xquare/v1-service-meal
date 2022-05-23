a0a09c7b16f9be17956a0cee39f9604d67136510
정지우 <jiwoourty@gmail.com>
2022-05-23 14:39:03 +0900

    :bookmark_tabs: :: add typeorm config module

---
+++ src/global/typeorm/typeorm-config.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: process.env.DATABASE_HOST,
+            port: +process.env.DATABASE_PORT,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+            logging: false,
+            entities: ["dist/**/adaptor/outbound/persistence/dms/**/*.entity{.ts,.js}"]
+        })
+    ]
+})
+export class TypeOrmConfigModule {}
