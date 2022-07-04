8c07ac14cba85f38a9c608111824f5f1fbc0ea1e
정지우 <jiwoourty@gmail.com>
2022-07-04 11:32:41 +0900

    ⚡️ :: (#4) core module
    add neis api module

--- src/core.module.ts
+++ src/core.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmConfigModule } from "@src/global/typeorm/typeorm-config.module";
 import { MealModule } from "@src/meal/meal.module";
+import { NeisApiModule } from "@src/global/neis-api/neis-api.module";
 
 @Module({
-    imports: [TypeOrmConfigModule, MealModule]
+    imports: [TypeOrmConfigModule, NeisApiModule, MealModule]
 })
 export class CoreModule {}
