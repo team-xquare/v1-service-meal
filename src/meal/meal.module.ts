1518de85cfca22e7c61c6a7ab985c8aeff4ec182
hwc9169 <hwc9169@gmail.com>
2022-08-25 20:26:27 +0900

    🧪 :: neis api 수정하기

--- src/meal/meal.module.ts
+++ src/meal/meal.module.ts
@@ -4,10 +4,10 @@ import { MealServiceImpl } from "@src/meal/application/meal.service.impl";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { MealEntity } from "@src/infrastructure/repository/entity/meal.entity";
 import { NeisApi } from "@src/infrastructure/api/meal-provider/neis_api/neis-api";
-import { MealServiceToken } from "./application/service/meal.service";
 import { MealProviderToken } from "@src/infrastructure/api/meal-provider/meal-provider";
 import { MealRepositoryToken } from "@src/infrastructure/repository/meal.repository";
 import { MealRepositoryImpl } from "@src/infrastructure/repository/meal.repository.impl";
+import { MealServiceToken } from "./application/meal.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([MealEntity])],
