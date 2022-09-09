9ee57060a8824ab9fcfd503e2c792a0d7b582480
HoWon <56785132+hwc9169@users.noreply.github.com>
2022-09-09 11:47:23 +0900

    🔀 :: sentry 추가 (#24)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from "@nestjs/common";
 import { CoreModule } from "./core.module";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as Sentry from "@sentry/node";
+import { SentryInterceptor } from "./infrastructure/sentry/interceptor";
 
 Date.prototype.toString = function () {
     const year = this.getFullYear();
@@ -17,6 +19,11 @@ async function bootstrap() {
     const config = new DocumentBuilder().setTitle("Meal service").addTag("meals").build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("meals/api", app, document);
+
+    Sentry.init({
+        dsn: process.env.SENTRY_ENV
+    });
+    app.useGlobalInterceptors(new SentryInterceptor());
 
     app.useGlobalPipes(
         new ValidationPipe({
