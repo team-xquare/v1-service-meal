550330455d90bf41ebffae83883a4e72a3d71c03
정대현 <eogus_2513@dsm.hs.kr>
2023-03-02 07:07:57 +0900

    ♻️ :: kcal 저장하도록 변경

--- src/infrastructure/repository/meal.repository.impl.ts
+++ src/infrastructure/repository/meal.repository.impl.ts
@@ -1,21 +1,22 @@
-import { MealRepository } from "./meal.repository";
-import { Meal, MealWithDate } from "@src/meal/domain/meal";
-import { InjectRepository } from "@nestjs/typeorm";
-import { MealEntity } from "@src/infrastructure/repository/entity/meal.entity";
-import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
+import {MealRepository} from "./meal.repository";
+import {Meal, MealWithDate} from "@src/meal/domain/meal";
+import {InjectRepository} from "@nestjs/typeorm";
+import {MealEntity} from "@src/infrastructure/repository/entity/meal.entity";
+import {Repository} from "typeorm";
+import {Injectable} from "@nestjs/common";
 
 @Injectable()
 export class MealRepositoryImpl implements MealRepository {
     constructor(
         @InjectRepository(MealEntity)
         private readonly mealRepository: Repository<MealEntity>
-    ) {}
+    ) {
+    }
 
     public async findMealByDate(date: Date): Promise<Meal> {
         const mealEntities: MealEntity[] = await this.mealRepository.find({
-            where: { date: date.toString() },
-            order: { type: "asc" }
+            where: {date: date.toString()},
+            order: {type: "asc"}
         });
 
         return new Meal().setBreakfast(mealEntities[0]?.meal).setLunch(mealEntities[1]?.meal).setDinner(mealEntities[2]?.meal).build();
@@ -23,26 +24,31 @@ export class MealRepositoryImpl implements MealRepository {
 
     async saveMeal(meal: Meal, date: Date): Promise<void> {
         const breakfastPromise =
-            meal.breakfast && this.mealRepository.save({ date: date.toString(), meal: meal.breakfast.join("||"), type: 0 });
-        const lunchPromise = meal.lunch && this.mealRepository.save({ date: date.toString(), meal: meal.lunch.join("||"), type: 1 });
-        const dinnerPromise = meal.dinner && this.mealRepository.save({ date: date.toString(), meal: meal.dinner.join("||"), type: 2 });
+            meal.breakfast && (await this.mealRepository.save(await this.entity(date, meal.breakfast, meal.breakfast_kcal, 0)));
+        const lunchPromise = meal.lunch && (await this.mealRepository.save(await this.entity(date, meal.lunch, meal.lunch_kcal, 1)));
+        const dinnerPromise = meal.dinner && (await this.mealRepository.save(await this.entity(date, meal.dinner, meal.dinner_kcal, 2)));
 
         await breakfastPromise;
         await lunchPromise;
         await dinnerPromise;
     }
 
+    private async entity(date: Date, menu: string[], kcal: string, type: number): Promise<MealEntity> {
+        return {
+            date: date.toString(),
+            meal: menu.concat(kcal).join("||"),
+            type
+        };
+    }
+
     async findMealByDateBetween(starDate: Date, endDate: Date): Promise<MealWithDate[]> {
         const meals = await this.mealRepository.query(
             `
-            select date,
-                   max(case when (type = 0) then meal end) breakfast,
-                   max(case when (type = 1) then meal end) lunch,
-                   max(case when (type = 2) then meal end) dinner
-              from meal
-             where date between ? and ?
-             group by date;
-        `,
+                select date, max (case when (type = 0) then meal end) breakfast, max (case when (type = 1) then meal end) lunch, max (case when (type = 2) then meal end) dinner
+                from meal
+                where date between ? and ?
+                group by date;
+            `,
             [starDate.toString(), endDate.toString()]
         );
 
